18b253c07f32318d77c6e68d4948ea258a03de85
Gopal1252 <guptagopal252@gmail.com>
2024-09-19 22:32:37 +0530

    added apis for get user data and delete user data

--- src/app.js
+++ src/app.js
@@ -17,6 +17,49 @@ app.post("/signup", async (req,res) => {
     }
 });
 
+//get user by email
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId : userEmail});
+        if(users.length === 0){
+            res.status(404).send("User with given email address not found!");
+        }
+        else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong!")
+    }
+})
+
+//get all users (feed) from the database
+app.get("/feed", async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+
+    }catch(err){
+        res.status(400).send("Something went wrong!")
+    }
+});
+
+//delete a user by id
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId;
+    try{
+        // const user = await User.findByIdAndDelete({id: userId});//this also works acc to docs
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully!")
+
+    }catch(err) {
+        res.status(400).send("Something went wrong!")
+    }
+})
+
+//
+
 connectDB().then(() => {
     console.log("DB connected successfully!");
     app.listen(3000, () => {
