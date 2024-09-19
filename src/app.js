d63cf47ef2c20d717b899127b0c99e1a8230cf5c
Gopal1252 <guptagopal252@gmail.com>
2024-09-19 21:55:47 +0530

    implemented dynamic signup

--- src/app.js
+++ src/app.js
@@ -3,22 +3,19 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req,res) => {
-    //creating a new instance of the user model 
-    const user = new User({
-        firstName : "Gopal",
-        lastName : "Gupta",
-        email : "gopal252@gmail.com",
-        password : "gopal@252"
-    });
+    // creating a new instance of the user model 
+    const user = new User(req.body);
     try{
         await user.save();
         res.send("User added successfully");
     }
-    catch{(err) => {
+    catch(err){
         res.status(400).send("Error saving the user" + err.message);
-    }}
-})
+    }
+});
 
 connectDB().then(() => {
     console.log("DB connected successfully!");
