53a6911e678a6a2c52e70630b1245af34bef52ff
Gopal1252 <guptagopal252@gmail.com>
2024-09-19 19:57:19 +0530

    added user schema and basic signup route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req,res) => {
+    //creating a new instance of the user model 
+    const user = new User({
+        firstName : "Gopal",
+        lastName : "Gupta",
+        email : "gopal252@gmail.com",
+        password : "gopal@252"
+    });
+    try{
+        await user.save();
+        res.send("User added successfully");
+    }
+    catch{(err) => {
+        res.status(400).send("Error saving the user" + err.message);
+    }}
+})
 
 connectDB().then(() => {
     console.log("DB connected successfully!");
@@ -10,3 +28,4 @@ connectDB().then(() => {
 }).catch((err) => {
     console.log("DB could not be connected!");
 })
+
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://guptagopal252:eNAJQAx3HRoIk8kW@cluster0.cj1xi.mongodb.net/");
+    await mongoose.connect("mongodb+srv://guptagopal252:eNAJQAx3HRoIk8kW@cluster0.cj1xi.mongodb.net/devConnect ");
 };
 
 module.exports = connectDB;
