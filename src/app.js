179aa5792b7209e59fc2bdfd553cdccdbae15d97
Gopal1252 <guptagopal252@gmail.com>
2024-09-20 11:09:13 +0530

    added api level vaidations on PATCH api

--- src/app.js
+++ src/app.js
@@ -59,11 +59,25 @@ app.delete("/user", async (req,res) => {
 })
 
 // update user data
-app.patch("/user", async (req,res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params?.userId;
     const data = req.body;
 
     try{
+        const ALLOWED_UPDATES = ["photoUrl","age","gender","about","skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k)=> 
+            ALLOWED_UPDATES.includes(k)
+        );
+    
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed!");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Cannot add more than 10 skills!")
+        }
+
         await User.findByIdAndUpdate({_id : userId},data,{
             returnDocument : "after",
             runValidators : true 
