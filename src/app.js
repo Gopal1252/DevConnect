62bbdb773f00810731742b76c1d5b2358821afac
Gopal1252 <guptagopal252@gmail.com>
2024-09-25 13:05:47 +0530

    some refactoring using mongoose schema methods

--- src/app.js
+++ src/app.js
@@ -47,12 +47,12 @@ app.post("/login", async (req,res) => {
             throw new Error("Invalid Credentials!")
         }
 
-        const isPasswordValid = await bcrypt.compare(password,user.password);
+        const isPasswordValid = await user.validatePassword(password);
 
         if(isPasswordValid){
 
             //create a JWT token
-            const token = jwt.sign({_id : user._id},"dev@Connect$252",{expiresIn: "1d",});
+            const token = await user.getJWT();
 
             //add the token to the cookie and send the response back to the user
             res.cookie("token", token)
