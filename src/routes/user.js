f8f955b254ce643b0446fa299323cea2fddd4fb9
Gopal1252 <guptagopal252@gmail.com>
2024-09-29 14:39:52 +0530

    added pagination to feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -81,10 +81,15 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
     }
 });
 
-userRouter.get("/feed", userAuth, async (req,res) => {
+userRouter.get("/feed?page=1&limit=10", userAuth, async (req,res) => {
     try{
 
         const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = ((limit>50)? 50 : limit);
+        const skip = (page-1)*limit;
 
         const connectionRequests = await ConnectionRequest.find({
             $or : [{fromUserId : loggedInUser._id}, {toUserId : loggedInUser._id}],
@@ -101,7 +106,7 @@ userRouter.get("/feed", userAuth, async (req,res) => {
                 {_id : { $nin : Array.from(hideUsersFromFeed) }},
                 {_id : { $ne : loggedInUser._id }}
             ],
-        }).select(USER_SAFE_DATA);
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
         res.send(users);
 
